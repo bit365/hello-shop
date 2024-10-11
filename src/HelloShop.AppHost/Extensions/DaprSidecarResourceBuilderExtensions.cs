81d8f1b3f95696b3df3764e0565da39d47296ab5
hello <stacks@outlook.com>
2024-10-11 21:30:57 +0800

    Aspire 集成 Dapr 和 RabbitMQ

---
+++ src/HelloShop.AppHost/Extensions/DaprSidecarResourceBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) HelloShop Corporation. All rights reserved.
+// See the license file in the project root for more information.
+
+using Aspire.Hosting.Dapr;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloShop.AppHost.Extensions
+{
+    public static class DaprSidecarResourceBuilderExtensions
+    {
+        public static IResourceBuilder<IDaprSidecarResource> WithReference(this IResourceBuilder<IDaprSidecarResource> builder, IResourceBuilder<IResourceWithConnectionString> resourceBuilder, int waitInSeconds = 5)
+        {
+            builder.WithAnnotation(new EnvironmentCallbackAnnotation(async context =>
+            {
+                var notificationService = context.ExecutionContext.ServiceProvider.GetRequiredService<ResourceNotificationService>();
+                await notificationService.WaitForResourceAsync(resourceBuilder.Resource.Name, KnownResourceStates.Running);
+                await Task.Delay(TimeSpan.FromSeconds(waitInSeconds));
+                var connectionStringName = resourceBuilder.Resource.ConnectionStringEnvironmentVariable ?? $"ConnectionStrings__{resourceBuilder.Resource.Name}";
+                context.EnvironmentVariables[connectionStringName] = new ConnectionStringReference(resourceBuilder.Resource, false);
+            }));
+
+            return builder;
+        }
+    }
+}
--- src/HelloShop.AppHost/Program.cs
+++ src/HelloShop.AppHost/Program.cs
@@ -1,24 +1,51 @@
 // Copyright (c) HelloShop Corporation. All rights reserved.
 // See the license file in the project root for more information.
 
+using HelloShop.AppHost.Extensions;
+using Aspire.Hosting.Dapr;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache", port: 6379);
 
-var identityService = builder.AddProject<Projects.HelloShop_IdentityService>("identityservice");
+var rabbitmq = builder.AddRabbitMQ("rabbitmq").WithManagementPlugin();
 
-var orderingService = builder.AddProject<Projects.HelloShop_OrderingService>("orderingservice").WithReference(identityService);
+var identityService = builder.AddProject<Projects.HelloShop_IdentityService>("identityservice")
+    .WithDaprSidecar();
 
-var productService = builder.AddProject<Projects.HelloShop_ProductService>("productservice").WithReference(identityService);
+DaprSidecarOptions daprSidecarOptions = new() { ResourcesPaths = ["DaprComponents"] };
 
-var basketService = builder.AddProject<Projects.HelloShop_BasketService>("basketservice").WithReference(identityService).WithReference(cache);
+var orderingService = builder.AddProject<Projects.HelloShop_OrderingService>("orderingservice")
+    .WithReference(identityService)
+    .WithDaprSidecar(options =>
+    {
+        options.WithOptions(daprSidecarOptions).WithReference(rabbitmq);
+    });
+
+var productService = builder.AddProject<Projects.HelloShop_ProductService>("productservice")
+    .WithReference(identityService)
+    .WithDaprSidecar(options =>
+    {
+        options.WithOptions(daprSidecarOptions).WithReference(rabbitmq);
+    }); ;
+
+var basketService = builder.AddProject<Projects.HelloShop_BasketService>("basketservice")
+    .WithReference(identityService)
+    .WithReference(cache)
+    .WithDaprSidecar(options =>
+    {
+        options.WithOptions(daprSidecarOptions).WithReference(rabbitmq);
+    });
 
 var apiservice = builder.AddProject<Projects.HelloShop_ApiService>("apiservice")
 .WithReference(identityService)
 .WithReference(orderingService)
 .WithReference(productService)
-.WithReference(basketService);
+.WithReference(basketService)
+.WithDaprSidecar();
 
-builder.AddProject<Projects.HelloShop_WebApp>("webapp").WithReference(apiservice);
+builder.AddProject<Projects.HelloShop_WebApp>("webapp")
+    .WithReference(apiservice)
+    .WithDaprSidecar();
 
 builder.Build().Run();
